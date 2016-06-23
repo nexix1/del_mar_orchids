5a446b82cae3f99b646ca3ce3b5f7597ce655961
Oliver Huang <oliverography@gmail.com>
2016-06-23 15:18:22 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,25 @@
+require "sinatra"
+
+get "/" do
+  erb :index
+end
+
+get "/about" do
+  erb :care1
+end
+
+get "/about" do
+  erb :care2
+end
+
+get "/about" do
+  erb :care3
+end
+
+get "/about" do
+  erb :care4
+end
+
+get "/about" do
+  erb :care5
+end
